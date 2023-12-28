91262cb426bd4a052a8d7c431c2199703bbf6038
EE <evelyneidsath@gmail.com>
2023-12-27 16:17:53 -0800

    render stored cities onto page on refresh, add fetch request function to history list

--- assets/script.js
+++ assets/script.js
@@ -57,12 +57,12 @@ function getCoords() {
 function renderCities() {
     var li = document.createElement("li");
 
-    li.textContent = cityName.value;
+    li.innerHTML = `<button onclick="seeHistory(event)" value="${cityName.value}">${cityName.value}</button>`;
     document.querySelector(".history").appendChild(li);
 }
 
 function storeCity() {
-    localStorage.setItem("savedCities", JSON.stringify(savedCities));
+    localStorage.setItem("storedCities", JSON.stringify(savedCities));
 };
 
 function weatherSearch() {
@@ -74,17 +74,64 @@ function weatherSearch() {
 }
 
 function init() {
-    for (var i = 0; i < savedCities.length; i++) {
-        var li = document.createElement("li");
+    const storedCities = JSON.parse(localStorage.getItem("storedCities"));
+    if (storedCities !== null) {
+        savedCities = storedCities;
+        for (var i = 0; i < storedCities.length; i++) {
+            var li = document.createElement("li");
 
-        li.textContent = savedCities[i];
-        li.setAttribute("data-index", i);
-        document.querySelector(".history").appendChild(li);
+            li.innerHTML = `<button onclick="seeHistory(event)" value="${storedCities[i]}">${storedCities[i]}</button>`;
+            document.querySelector(".history").appendChild(li);
+        }
     }
 }
+init();
 
-const storedCityName = JSON.parse(localStorage.getItem("storedCity"));
+function seeHistory(event) {
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + event.target.value + "&limit=3&appid=841bff87cafe164f0f5f33bd44701bfc";
 
-//new api request for when user clicks on history:
+    fetch(requestUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                const lat = data[0].lat;
+                const lon = data[0].lon;
+                nameEl.innerHTML = data[0].name + ", " + data[0].state + " " + today;
+                function getCurrentWeather() {
+                    let requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=841bff87cafe164f0f5f33bd44701bfc&units=imperial";
+                
+                    fetch(requestUrl).then(function (response) {
+                        if (response.ok) {
+                            response.json().then(function (data) {
+                                console.log(data);
+                                dataEl.innerHTML = `Current temp: ${data.main.temp} F<br>High: ${data.main.temp_max} F<br>Low: ${data.main.temp_min} F<br>Wind: ${data.wind.speed} mph<br>Humidity: ${data.main.humidity}% rh`;
+                            })
+                        }
+                    })
+                };
+                getCurrentWeather();
+                function getWeatherForecast() {
+                    let requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=841bff87cafe164f0f5f33bd44701bfc&units=imperial&cnt";
+                
+                    fetch(requestUrl).then(function (response) {
+                        if (response.ok) {
+                            response.json().then(function (data) {
+                                console.log(data);
+                                document.querySelector(".day1").innerHTML = `${data.list[4].dt_txt}<br>Temp: ${data.list[4].main.temp} F<br>Wind: ${data.list[4].wind.speed} mph<br>Humidity: ${data.list[4].main.humidity}% rh`;
 
-"http://api.openweathermap.org/geo/1.0/direct?q=" + storedCityName + "&limit=3&appid=841bff87cafe164f0f5f33bd44701bfc";
+                                document.querySelector(".day2").innerHTML = `${data.list[12].dt_txt}<br>Temp: ${data.list[12].main.temp} F<br>Wind: ${data.list[12].wind.speed} mph<br>Humidity: ${data.list[12].main.humidity}% rh`;
+
+                                document.querySelector(".day3").innerHTML = `${data.list[20].dt_txt}<br>Temp: ${data.list[20].main.temp} F<br>Wind: ${data.list[20].wind.speed} mph<br>Humidity: ${data.list[20].main.humidity}% rh`;
+
+                                document.querySelector(".day4").innerHTML = `${data.list[28].dt_txt}<br>Temp: ${data.list[28].main.temp} F<br>Wind: ${data.list[28].wind.speed} mph<br>Humidity: ${data.list[28].main.humidity}% rh`;
+
+                                document.querySelector(".day5").innerHTML = `${data.list[36].dt_txt}<br>Temp: ${data.list[36].main.temp} F<br>Wind: ${data.list[36].wind.speed} mph<br>Humidity: ${data.list[36].main.humidity}% rh`;
+                            })
+                        }
+                    })
+                };
+                getWeatherForecast();
+            })
+        }
+    })
+};
